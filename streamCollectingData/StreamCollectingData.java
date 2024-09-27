76f1283f798cce7ef8ae4e7ae074028ee9e6df8e
bion0625 <bion0625@gmail.com>
2024-09-27 09:44:41 +0900

    6.1 컬렉터란 무엇인가?

---
+++ streamCollectingData/StreamCollectingData.java
@@ -0,0 +1,49 @@
+package streamCollectingData;
+
+import streamUsing.Trader;
+import streamUsing.Transaction;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamCollectingData {
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        Map<Trader, List<Transaction>> transactionByTraders1
+                = new HashMap<>();
+
+        for (Transaction transaction : transactions) {
+            Trader trader = transaction.getTrader();
+            List<Transaction> transactionsForTrader
+                    = transactionByTraders1.get(trader);
+            if (transactionsForTrader == null) {
+                transactionsForTrader = new ArrayList<>();
+                transactionByTraders1.put(trader, transactionsForTrader);
+            }
+            transactionsForTrader.add(transaction);
+        }
+        System.out.println("transactionByTraders1: " + transactionByTraders1);
+
+        Map<Trader, List<Transaction>> transactionByTraders2
+                = transactions.stream().collect(Collectors.groupingBy(Transaction::getTrader));
+        System.out.println("transactionByTraders2: " + transactionByTraders2);
+
+        Stream<Transaction> transactionStream = transactions.stream();
+        List<Transaction> transactions1 = transactionStream.collect(Collectors.toList());
+        System.out.println("transactions1: " + transactions1);
+    }
+}
