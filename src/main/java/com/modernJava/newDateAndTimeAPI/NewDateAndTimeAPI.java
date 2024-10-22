a0fe4c5addf7ccd036d6aaa63932c07a0eb47a78
bion0625 <bion0625@gmail.com>
2024-10-22 09:59:22 +0900

    12.2 날짜 조정, 파싱, 포매팅

--- src/main/java/com/modernJava/newDateAndTimeAPI/NewDateAndTimeAPI.java
+++ src/main/java/com/modernJava/newDateAndTimeAPI/NewDateAndTimeAPI.java
@@ -1,9 +1,14 @@
 package com.modernJava.newDateAndTimeAPI;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
+import java.util.Locale;
 
 public class NewDateAndTimeAPI {
     public static void main(String[] args) {
@@ -100,5 +105,110 @@ public class NewDateAndTimeAPI {
         System.out.println("tenDays2: " + tenDays2);
         System.out.println("threeWeeks: " + threeWeeks);
         System.out.println("twoYearsSixMonthOneDay: " + twoYearsSixMonthOneDay);
+
+        LocalDate date5 = LocalDate.of(2017, 9, 21);
+        LocalDate date6 = date5.withYear(2011);
+        LocalDate date7 = date6.withDayOfMonth(25);
+        LocalDate date8 = date7.with(ChronoField.MONTH_OF_YEAR, 2);
+        System.out.println("date5: " + date5);
+        System.out.println("date6: " + date6);
+        System.out.println("date7: " + date7);
+        System.out.println("date8: " + date8);
+
+        LocalDate date9 = LocalDate.of(2017, 9, 21);
+        LocalDate date10 = date9.plusWeeks(1);
+        LocalDate date11 = date10.minusYears(6);
+        LocalDate date12 = date11.plus(6, ChronoUnit.MONTHS);
+        System.out.println("date9: " + date9);
+        System.out.println("date10: " + date10);
+        System.out.println("date11: " + date11);
+        System.out.println("date12: " + date12);
+
+        LocalDate date13 = LocalDate.of(2014, 3, 18);
+        LocalDate date14 = date13.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+        LocalDate date15 = date14.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println("date13: " + date13);
+        System.out.println("date14: " + date14);
+        System.out.println("date15: " + date15);
+
+        LocalDate date16 = LocalDate.of(2024, 10, 18);
+        LocalDate date17 = date16.with(new NextWorkingDay());
+        System.out.println("date16: " + date16);
+        System.out.println("date17: " + date17);
+
+        LocalDate date18 = LocalDate.of(2024, 10, 18);
+        LocalDate date19 = date18.with(temporal -> {
+            DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+            int dayToAdd = 1;
+            if (dow == DayOfWeek.FRIDAY) dayToAdd = 3;
+            else if (dow == DayOfWeek.SATURDAY) dayToAdd = 2;
+            return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+        });
+        System.out.println("date18: " + date18);
+        System.out.println("date19: " + date19);
+
+        TemporalAdjuster nextWorkingDay = TemporalAdjusters.ofDateAdjuster(
+                temporal -> {
+                    DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+                    int dayToAdd = 1;
+                    if (dow == DayOfWeek.FRIDAY) dayToAdd = 3;
+                    else if (dow == DayOfWeek.SATURDAY) dayToAdd = 2;
+                    return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+        });
+        LocalDate date20 = LocalDate.of(2024, 10, 18);
+        LocalDate date21 = date20.with(nextWorkingDay);
+        System.out.println("date20: " + date20);
+        System.out.println("date21: " + date21);
+
+        LocalDate date22 = LocalDate.of(2014, 3, 18);
+        String s1 = date22.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date22.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println("date22: " + date22);
+        System.out.println("s1: " + s1);
+        System.out.println("s2: " + s2);
+
+        LocalDate date23 = LocalDate.parse("20140318", DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate date24 = LocalDate.parse("2014-03-18", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println("date23: " + date23);
+        System.out.println("date24: " + date24);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date25 = LocalDate.of(2014, 3, 18);
+        String formattedDate1 = date25.format(formatter);
+        LocalDate date26 = LocalDate.parse(formattedDate1, formatter);
+        System.out.println("date25: " + date25);
+        System.out.println("formattedDate1: " + formattedDate1);
+        System.out.println("date26: " + date26);
+
+        DateTimeFormatter italianFormatter1 = DateTimeFormatter.ofPattern("d. MMMM yyyy", Locale.ITALIAN);
+        LocalDate date27 = LocalDate.of(2014, 3, 18);
+        String formattedDate2 = date27.format(italianFormatter1);
+        LocalDate date28 = LocalDate.parse(formattedDate2, italianFormatter1);
+        System.out.println("date27: " + date27);
+        System.out.println("formattedDate2: " + formattedDate2);
+        System.out.println("date28: " + date28);
+
+        DateTimeFormatter koreanFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 dd일", Locale.KOREA);
+        LocalDate date29 = LocalDate.of(2014, 3, 18);
+        String formattedDate3 = date29.format(koreanFormatter);
+        LocalDate date30 = LocalDate.parse(formattedDate3, koreanFormatter);
+        System.out.println("date29: " + date29);
+        System.out.println("formattedDate3: " + formattedDate3);
+        System.out.println("date30: " + date30);
+
+        DateTimeFormatter italianFormatter2 = new DateTimeFormatterBuilder()
+                .appendText(ChronoField.DAY_OF_MONTH)
+                .appendLiteral(". ")
+                .appendText(ChronoField.MONTH_OF_YEAR)
+                .appendLiteral(" ")
+                .appendText(ChronoField.YEAR)
+                .parseCaseInsensitive()
+                .toFormatter(Locale.ITALIAN);
+        LocalDate date31 = LocalDate.of(2014, 3, 18);
+        String formattedDate4 = date31.format(italianFormatter2);
+        LocalDate date32 = LocalDate.parse(formattedDate4, italianFormatter2);
+        System.out.println("date31: " + date31);
+        System.out.println("formattedDate4: " + formattedDate4);
+        System.out.println("date32: " + date32);
     }
 }
