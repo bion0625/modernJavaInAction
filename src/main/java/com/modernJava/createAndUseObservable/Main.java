4ce97f07f0f419ce666826b65a138337531db356
bion0625 <bion0625@gmail.com>
2024-11-06 09:41:50 +0900

    17.3.2 Observable을 변환하고 합치기

--- src/main/java/com/modernJava/createAndUseObservable/Main.java
+++ src/main/java/com/modernJava/createAndUseObservable/Main.java
@@ -3,7 +3,9 @@ package com.modernJava.createAndUseObservable;
 import com.modernJava.firstReactiveApplication.TempInfo;
 import io.reactivex.Observable;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +15,11 @@ public class Main {
 //        onePerSec.subscribe(i -> System.out.println(TempInfo.fetch("New York")));
 //        onePerSec.blockingSubscribe(i -> System.out.println(TempInfo.fetch("New York")));
 
-        Observable<TempInfo> observable = getTemperature("New York");
+//        Observable<TempInfo> observable = getTemperature("New York");
+//        Observable<TempInfo> observable = getCelsiusTemperature("New York");
+//        Observable<TempInfo> observable = getNegativeTemperature("New York");
+
+        Observable<TempInfo> observable = getCelsiusTemperatures("New York", "Chicago", "San Francisco");
         observable.blockingSubscribe(new TempObserver());
     }
 
@@ -31,5 +37,22 @@ public class Main {
                 }
             }
         }));
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperature(String town) {
+        return getTemperature(town)
+                .map(temp -> new TempInfo(temp.getTown(),
+                        (temp.getTemp() - 32) * 5 / 9));
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperatures(String... towns) {
+        return Observable.merge(Arrays.stream(towns)
+                .map(Main::getCelsiusTemperature)
+                .collect(Collectors.toList()));
+    }
+
+    public static Observable<TempInfo> getNegativeTemperature(String town) {
+        return getTemperature(town)
+                .filter(temp -> temp.getTemp() < 0);
     }
 }
