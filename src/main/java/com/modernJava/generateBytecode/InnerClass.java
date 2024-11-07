0bc755796e5d8d0ef22d22b75b84e6f84471e05c
bion0625 <bion0625@gmail.com>
2024-11-07 17:08:27 +0900

    D.2 바이트 코드 생성

---
+++ src/main/java/com/modernJava/generateBytecode/InnerClass.java
@@ -0,0 +1,12 @@
+package com.modernJava.generateBytecode;
+
+import java.util.function.Function;
+
+public class InnerClass {
+    Function<Object, String> f = new Function<Object, String>() {
+        @Override
+        public String apply(Object obj) {
+            return obj.toString();
+        }
+    };
+}
